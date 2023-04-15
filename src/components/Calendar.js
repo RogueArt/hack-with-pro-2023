401efd2c140247547d5e1280542f551851da77a7
Rogue Art <rogueart.dev@outlook.com>
2023-04-15 16:01:41 -0700

    Add display for events

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,6 +1,11 @@
 import React, { useState } from 'react'
 import './Calendar.css'
 
+// If an hour is the start of an event, then put the name there and oclor
+// If the next hour is part of an event, then background color only
+
+// 5 PM must be between 4 PM and 6 PM
+
 const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
 
 function Calendar({ events, workingHours }) {
@@ -26,8 +31,9 @@ function Calendar({ events, workingHours }) {
   const endHour = workingHours.endHour
   const hours = []
   for (let i = startHour; i <= endHour; i++) {
-    hours.push(getFormattedHour(i))
+    hours.push(i)
   }
+  const fmtHours = hours.map(hour => getFormattedHour(hour))
 
   const getEventStyle = (startHour, endHour) => {
     const start = hours.indexOf(startHour)
@@ -50,24 +56,45 @@ function Calendar({ events, workingHours }) {
       <div className="calendar-body">
         {hours.map(hour => (
           <div key={hour} className="calendar-row">
-            <div className="calendar-hour">{hour}</div>
+            <div className="calendar-hour">{getFormattedHour(hour)}</div>
+            
+            {/* Go through each day for that  */}
             {days.map(day => {
               const isCellSelected =
                 selectedCell &&
                 selectedCell.day === day &&
                 selectedCell.hour === hour
-              const eventsForCell = events.filter(
-                event => event.day === day && event.startHour === hour
-              )
+
+              // If there's an event for this day and time, then set the style
+              const eventsForCell = events.filter((event) => {
+                return event.daysOfWeek.includes(day) && event.startHour <= hour && hour < event.endHour
+              })
+              const hasEvent = eventsForCell.length > 0
+
+              let eventStyle = {}
+              let isEventStart = false
+              if (hasEvent) {
+                const event = eventsForCell[0]
+                isEventStart = event.startHour === hour
+                eventStyle = { backgroundColor: event.color, border: event.color }
+              }
+
+              const cellEvent = eventsForCell[0]
+              // const isEventStart = cellEvent.startHour === hour
+
+              // console.log(eventsForCell)
+
               return (
                 <div
                   key={`${day}-${hour}`}
-                  className={`calendar-cell ${
-                    isCellSelected ? 'selected' : ''
-                  }`}
-                  onClick={() => handleCalendarClick(day, hour)}
+                  className={`calendar-cell ${hasEvent && `calendar-event`}`}
+                  style={eventStyle}
+                  // onClick={() => handleCalendarClick(day, hour)}
                 >
-                  {eventsForCell.map(event => (
+                  {eventsForCell.map(event => {
+                    return <p className={'calendar-event-text'}>{isEventStart && event.name}</p>
+                  })}
+                  {/* {eventsForCell.map(event => (
                     <div
                       key={event.name}
                       className="calendar-event"
@@ -75,7 +102,7 @@ function Calendar({ events, workingHours }) {
                     >
                       {event.name}
                     </div>
-                  ))}
+                  ))} */}
                 </div>
               )
             })}
