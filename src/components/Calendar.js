0b980f45e7dc935adc8a5c1f946402a11c4172cf
Rogue Art <rogueart.dev@outlook.com>
2023-04-15 13:56:08 -0700

    Create Calendar component that takes in events and working hours

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,81 +1,89 @@
-import React, { Component } from 'react';
-import { DayPilot, DayPilotCalendar } from '@daypilot/daypilot-lite-react';
+import React, { useState } from 'react'
+import './Calendar.css'
 
-class Calendar extends Component {
-  constructor(props) {
-    super(props);
+const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
 
-    this.state = {
-      events: [],
-    };
+function Calendar({ events, workingHours }) {
+  const [selectedCell, setSelectedCell] = useState(null)
+
+  const handleCalendarClick = (day, hour) => {
+    setSelectedCell({ day, hour })
   }
 
-  componentDidMount() {
-    // You can load events from a server here
+  const getFormattedHour = hour => {
+    if (hour === 0) {
+      return '12 AM'
+    } else if (hour < 12) {
+      return `${hour} AM`
+    } else if (hour === 12) {
+      return '12 PM'
+    } else {
+      return `${hour - 12} PM`
+    }
   }
 
-  handleTimeRangeSelected = async (args) => {
-    const name = prompt("New event name:", "Event");
-    if (!name) return;
+  const startHour = workingHours.startHour
+  const endHour = workingHours.endHour
+  const hours = []
+  for (let i = startHour; i <= endHour; i++) {
+    hours.push(getFormattedHour(i))
+  }
 
-    const e = {
-      start: args.start,
-      end: args.end,
-      id: DayPilot.guid(),
-      text: name,
-    };
+  const getEventStyle = (startHour, endHour) => {
+    const start = hours.indexOf(startHour)
+    const end = hours.indexOf(endHour)
+    return {
+      gridRow: `${start + 2} / ${end + 3}`,
+    }
+  }
 
-    this.setState((prevState) => {
-      return { events: [...prevState.events, e] };
-    });
-  };
-
-  handleEventMove = async (args) => {
-    this.setState((prevState) => {
-      return {
-        events: prevState.events.map((event) => {
-          if (event.id === args.e.id()) {
-            event.start = args.newStart;
-            event.end = args.newEnd;
-          }
-          return event;
-        }),
-      };
-    });
-  };
-
-  handleEventResize = async (args) => {
-    this.setState((prevState) => {
-      return {
-        events: prevState.events.map((event) => {
-          if (event.id === args.e.id()) {
-            event.start = args.newStart;
-            event.end = args.newEnd;
-          }
-          return event;
-        }),
-      };
-    });
-  };
-
-  render() {
-    const { events } = this.state;
-
-    const config = {
-      timeRangeSelectedHandling: "Enabled",
-      onTimeRangeSelected: this.handleTimeRangeSelected,
-      eventMoveHandling: "Update",
-      onEventMove: this.handleEventMove,
-      eventResizeHandling: "Update",
-      onEventResize: this.handleEventResize,
-    };
-
-    return (
-      <div>
-        <DayPilotCalendar {...config} events={events} />
+  return (
+    <div className="calendar">
+      <div className="calendar-header">
+        <div className="calendar-header-day" />
+        {days.map(day => (
+          <div key={day} className="calendar-header-day">
+            {day}
+          </div>
+        ))}
       </div>
-    );
-  }
+      <div className="calendar-body">
+        {hours.map(hour => (
+          <div key={hour} className="calendar-row">
+            <div className="calendar-hour">{hour}</div>
+            {days.map(day => {
+              const isCellSelected =
+                selectedCell &&
+                selectedCell.day === day &&
+                selectedCell.hour === hour
+              const eventsForCell = events.filter(
+                event => event.day === day && event.startHour === hour
+              )
+              return (
+                <div
+                  key={`${day}-${hour}`}
+                  className={`calendar-cell ${
+                    isCellSelected ? 'selected' : ''
+                  }`}
+                  onClick={() => handleCalendarClick(day, hour)}
+                >
+                  {eventsForCell.map(event => (
+                    <div
+                      key={event.name}
+                      className="calendar-event"
+                      style={getEventStyle(event.startHour, event.endHour)}
+                    >
+                      {event.name}
+                    </div>
+                  ))}
+                </div>
+              )
+            })}
+          </div>
+        ))}
+      </div>
+    </div>
+  )
 }
 
-export default Calendar;
+export default Calendar
