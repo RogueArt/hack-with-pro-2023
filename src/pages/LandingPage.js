510de1294938dbe4e2348ca7ae2006b83cb8be78
Jason Wan <njasow@g.ucla.edu>
2023-04-15 17:59:15 -0700

    work on landing page

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,9 +1,19 @@
 import React, { useState } from 'react'
+import '../pages/LandingPage.css'
 
 export function LandingPage() {
   return (
     <div>
-      <p>Hello landing page!</p>
+      <br></br>
+      <h2>Welcome to</h2>
+      <h1><span class="magic">Smart Schedule</span></h1>
+      <h3>"The lost time is never found again" - Benjamin Franklin</h3>
+      <div class = "startb">
+        <p class = "start">Start</p>
+      </div>
+      <div id = "image-track">
+        
+      </div>
     </div>
   )
 }
