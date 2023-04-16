a53b667cb0f9698c35d45d5fef028b2fdae97b7f
Rogue Art <rogueart.dev@outlook.com>
2023-04-15 18:03:23 -0700

    Finished mood tracker functionality

--- src/pages/MoodPage.js
+++ src/pages/MoodPage.js
@@ -1,95 +1,107 @@
 import React, { useState } from 'react'
-import "./MoodPage.css"
+
+const options = {
+  Happy: [
+    { value: '', label: '' },
+    { value: 'relaxed', label: 'Relaxed' },
+    { value: 'elated', label: 'Elated' },
+    { value: 'confident', label: 'Confident' },
+    { value: 'creative', label: 'Creative' },
+    { value: 'unstoppable', label: 'Unstoppable' },
+  ],
+  Neutral: [
+    { value: '', label: '' },
+    { value: 'bored', label: 'Bored' },
+    { value: 'unfazed', label: 'Unfazed' },
+    { value: 'unfocused', label: 'Unfocused' },
+    { value: 'satisfied', label: 'Satisfied' },
+    { value: 'determined', label: 'Determined' },
+  ],
+  Sad: [
+    { value: '', label: '' },
+    { value: 'depressed', label: 'Depressed' },
+    { value: 'gloomy', label: 'Gloomy' },
+    { value: 'overwhelmed', label: 'Overwhelmed' },
+    { value: 'grumpy', label: 'Grumpy' },
+    { value: 'worried', label: 'Worried' },
+  ],
+}
 
 export function MoodPage() {
-    const [selectedRadioBtn, setSelectedRadioBtn] = React.useState('Neutral');
-    const isRadioSelected = (value ) => selectedRadioBtn == value;
-    const handleRadioClick = (e) => setSelectedRadioBtn(e.currentTarget.value);
+  const [selectedMood, setSelectedMood] = useState('Neutral')
+  const [selectedOptions, setSelectedOptions] = useState(options.Neutral)
+  const [selectedFeeling, setSelectedFeeling] = useState('')
 
-    const optionsHappy = [
-        { value: "relaxed", label: "Relaxed" },
-        { value: "elated", label: "Elated" },
-        { value: "confident", label: "Confident" },
-        { value: "creative", label: "Creative" },
-        { value: "unstoppable", label: "Unstoppable" }
-    ]
+  const isRadioSelected = value => selectedMood === value
+  const handleRadioClick = e => {
+    setSelectedMood(e.currentTarget.value)
+    setSelectedOptions(options[e.currentTarget.value])
+  }
 
-    const optionsNeutral = [
-        { value: "relaxed", label: "Relaxed" },
-        { value: "unfazed", label: "Unfazed" },
-        { value: "unfocused", label: "Unfocused" },
-        { value: "satisfied", label: "Satisfied" },
-        { value: "determined", label: "Determined" }
-    ]
+  function handleSelectDropdown(e) {
+    setSelectedFeeling(e.target.value)
+  }
 
-    const optionsSad = [
-        { value: "depressed", label: "Depressed" },
-        { value: "gloomy", label: "Gloomy" },
-        { value: "overwhelmed", label: "Overwhelmed" },
-        { value: "grumpy", label: "Grumpy" },
-        { value: "worried", label: "Worried" }
-    ]
+  function submitMood(e) {
+    const object = {
+      mood: selectedMood,
+      feeling: selectedFeeling,
+    }
+    return object
+  }
 
-    return (
-        <div id={'mood-page-container'}>
-            <p>Good, Neutral, Sad</p>
-            <input 
-                type= "radio"
-                name = "radio button general mood"
-                value = "Happy"
-                checked = {
-                    isRadioSelected("Happy")
-                }
-                onChange = {handleRadioClick}
-            />
-            <input 
-                type= "radio"
-                name = "radio button general mood"
-                value = "Neutral"
-                checked = {
-                    isRadioSelected("Neutral")
-                }
-                onChange = {handleRadioClick}
-            />
-            <input 
-                type= "radio"
-                name = "radio button general mood"
-                value = "Sad"
-                checked = {
-                    isRadioSelected("Sad")
-                }
-                onChange = {handleRadioClick}
-            />
-        
-        <h1>Checkbox select</h1>
+  return (
+    <div id={'mood-page-container'}>
+      <h1>Mood Tracker</h1>
 
-        <form id="make_checkbox_select">
+      <div style={{ display: 'flex', flexDirection: 'column' }}>
+        <label>
+          😊 Happy:
+          <input
+            type="radio"
+            name="radio button general mood"
+            value="Happy"
+            checked={isRadioSelected('Happy')}
+            onChange={handleRadioClick}
+          />
+        </label>
 
-        <select name="make">
-            <option data-count="2" value="relaxed">Relaxed</option>
-            <option data-count="23" value="elated">Elated</option>
-            <option data-count="433" value="confident">Confident</option>
-            <option data-count="45" value="creative">Creative</option>
-            <option data-count="476" value="unstoppable">Unstoppable</option>
-            <option data-count="78" value="Dodge">Dodge</option>
-            <option data-count="123" value="Fiat">Fiat</option>
-            <option data-count="32" value="Ford">Ford</option>
-            <option data-count="3" value="Honda">Honda</option>
-            <option data-count="342" value="Hyundai">Hyundai</option>
-            <option data-count="45" value="Isuzu">Isuzu</option>
-            <option data-count="653" value="Jaguar">Jaguar</option>
-            <option data-count="3" value="Jeep">Jeep</option>
-            <option data-count="23" value="Kia">Kia</option>
-            <option data-count="5656" value="Lamborghini">Lamborghini</option>
-            <option data-count="2133" value="Land Rover">Land Rover</option>
-            
+        <label>
+          😐 Neutral:
+          <input
+            type="radio"
+            name="radio button general mood"
+            value="Neutral"
+            checked={isRadioSelected('Neutral')}
+            onChange={handleRadioClick}
+          />
+        </label>
+
+        <label>
+          😔 Sad:
+          <input
+            type="radio"
+            name="radio button general mood"
+            value="Sad"
+            checked={isRadioSelected('Sad')}
+            onChange={handleRadioClick}
+          />
+        </label>
+      </div>
+
+      <form id="make_checkbox_select">
+        <select name="make" onChange={e => handleSelectDropdown(e)} required>
+          {selectedOptions.map(opt => {
+            return <option value={opt.value}>{opt.label}</option>
+          })}
         </select>
-        
-        <input type="submit" />
-        
-        </form>
-    
-        {/* <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+      </form>
+
+      <button onClick={submitMood()}>
+        Submit
+      </button>
+
+      {/* <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
 
         
 
@@ -111,10 +123,6 @@ export function MoodPage() {
         
             });
                  */}
-        
-
-
-
-        </div>
-    )
+    </div>
+  )
 }
